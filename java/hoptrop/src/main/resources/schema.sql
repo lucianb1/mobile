d865fe9edc829ae7535bd64c00e77693e765f4fd
Lucian Baciu <lucian.baciu@servustech.com>
2016-12-04 21:27:52 +0200

    main project

---
+++ java/hoptrop/src/main/resources/schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE accounts (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	email VARCHAR(100) NOT NULL,
+	
+	
+)
+
+accounts (id, email, password, )
+
+company (id, )
+
+domains (id, name)
+
+members (id, account_id)
