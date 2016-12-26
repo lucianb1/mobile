4f70d5787b678a06937ee3db6ab634cd2ee89c36
Lucian Baciu <lucian.baciu@servustech.com>
2016-12-26 19:27:45 +0200

    timetable serializer

--- java/hoptrop/src/main/java/ro/hoptrop/model/timetable/WeekTimetableSerializer.java
+++ java/hoptrop/src/main/java/ro/hoptrop/model/timetable/WeekTimetableSerializer.java
@@ -1,5 +1,8 @@
 package ro.hoptrop.model.timetable;
 
+import java.nio.ByteBuffer;
+import java.nio.ShortBuffer;
+
 /**
  * Created by Luci on 18-Dec-16.
  */
@@ -9,11 +12,26 @@ public class WeekTimetableSerializer {
     private static final int COLUMNS = 4 * 24;
 
     public static byte[] serialize(short[][] timetable) {
-        return null;
+        ByteBuffer buffer = ByteBuffer.allocate(ROWS * COLUMNS * 2);
+        ShortBuffer shortBuffer = buffer.asShortBuffer();
+        for (int i = 0; i < ROWS; i++) {
+            shortBuffer.put(timetable[i]);
+        }
+        return buffer.array();
     }
 
     public static short[][] deserialize(byte[] byteArray) {
-        return null;
+        int bytesCount = ROWS * COLUMNS * 2;
+        if (byteArray.length != bytesCount) {
+            throw new RuntimeException("Unsupported format");
+        }
+        short[][] result = new short[ROWS][COLUMNS];
+        for (int i = 0; i < ROWS; i++) {
+            ByteBuffer buffer = ByteBuffer.wrap(byteArray, i * COLUMNS * 2, COLUMNS * 2);
+            ShortBuffer shortBuffer = buffer.asShortBuffer();
+            shortBuffer.get(result[i]);
+        }
+        return result;
     }
 
 }
