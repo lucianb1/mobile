255b4ac7a01cecfa0d6c4a1cdbddcdbd177746d0
Lucian Baciu <lucian.baciu@servustech.com>
2017-03-09 18:17:45 +0200

    fixes

--- java/hoptrop/src/main/java/ro/hoptrop/controller/DomainController.java
+++ java/hoptrop/src/main/java/ro/hoptrop/controller/DomainController.java
@@ -2,13 +2,11 @@ package ro.hoptrop.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import ro.hoptrop.model.domain.CompanyDomain;
-import ro.hoptrop.security.PrincipalUser;
 import ro.hoptrop.service.DomainService;
 import ro.hoptrop.web.response.domain.DomainsJsonResponse;
 
@@ -25,8 +23,7 @@ public class DomainController {
 
 
     @RequestMapping(value="/domain/companies", method = RequestMethod.GET)
-    public DomainsJsonResponse getCompanyDomains(@AuthenticationPrincipal PrincipalUser user) {
-        user.getAuthorities().forEach(item -> System.out.println(item.getAuthority()));
+    public DomainsJsonResponse getCompanyDomains() {
         List<CompanyDomain> domains = domainService.getAllCompanyDomains();
         return new DomainsJsonResponse().setDomains(domains);
     }
